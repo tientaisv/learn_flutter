720301e45aeb2a62305b2a816a0d7712c3606fbc
tientaisv <nguyentaitien@gmail.com>
2023-07-27 22:14:47 +0700

    learn container 27-7

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,43 @@ void main() {
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blueAccent,
-          title: const Text('logo'),
+          title: Container(
+            child: const Text(
+              'Flutter',
+              style: TextStyle(),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          centerTitle: false,
         ),
-        body: const Center(child: Text('hello home!')),
+        body: MyWidget(),
       ),
     ),
     debugShowCheckedModeBanner: false,
   ));
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //color: Colors.blue,
+      margin: const EdgeInsets.all(15),
+      // ignore: sort_child_properties_last
+      child: const Text(
+        'Thử nghiệm',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 14,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      padding: const EdgeInsets.all(50),
+      decoration: const BoxDecoration(
+          color: Colors.blue,
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(8), topRight: Radius.circular(8))),
+    );
+  }
 }
